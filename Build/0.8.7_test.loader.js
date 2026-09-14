33ce59375a7c7fc94f11a7b5c56c0909cec587e3
Bogdan <b.konnov00@gmail.com>
2026-09-14 14:09:59 +0300

    0.8.6

--- Build/0.8.7_test.loader.js
+++ Build/0.8.7_test.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 53874802,
+    wasmFileSize: 55040939,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
